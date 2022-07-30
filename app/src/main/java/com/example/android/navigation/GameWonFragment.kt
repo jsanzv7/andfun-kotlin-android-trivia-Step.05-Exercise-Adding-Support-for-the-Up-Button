45e11adee266c7aa141fe1d8d522266ec9dfc9df
jsanz <jsanzv7@gmail.com>
2022-07-29 22:24:53 -0400

    commit

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -43,9 +43,9 @@ class GameWonFragment : Fragment() {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
         binding.nextMatchButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+            view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
-        var args = GameWonFragmentArgs.fromBundle(arguments)
+        var args = GameWonFragmentArgs.fromBundle(requireArguments())
         Toast.makeText(context,
         "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
         Toast.LENGTH_LONG).show()
